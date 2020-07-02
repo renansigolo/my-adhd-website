db82d93a818241d645bc87b7eb03eac426bb6135
Renan Sigolo <renan.sigolo@gmail.com>
2020-07-02 15:32:24 +1000

    feat: add disclaimer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,37 @@
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Grid, Typography, Box } from '@material-ui/core'
+import { Grid, Typography, Box, Container } from '@material-ui/core'
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
 function Footer() {
   return (
     <footer id="footer">
-      <Box display="flex" height="60px">
-        <Grid container justify="center" alignItems="center">
-          <Typography variant="caption">
-            <FormattedMessage
-              id="footer.title"
-              defaultMessage="Made with {heartIcon} by Renan Sigolo."
-              values={{ heartIcon: <FontAwesomeIcon icon={faHeart} /> }}
-            />
-          </Typography>
-        </Grid>
-      </Box>
+      <Container>
+        <Box display="flex" height="120px">
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            direction="column"
+          >
+            <Typography variant="caption" align="center" gutterBottom>
+              <FormattedMessage
+                id="footer.disclaimer"
+                defaultMessage="This app is for educational purposes only and does not reflect a medical opinion.ADHD can only be diagnosed by a medical professional!"
+                values={{ br: <br /> }}
+              />
+            </Typography>
+            <Typography variant="caption" align="center">
+              <FormattedMessage
+                id="footer.title"
+                defaultMessage="Made with {heartIcon} by Renan Sigolo."
+                values={{ heartIcon: <FontAwesomeIcon icon={faHeart} /> }}
+              />
+            </Typography>
+          </Grid>
+        </Box>
+      </Container>
     </footer>
   )
 }
