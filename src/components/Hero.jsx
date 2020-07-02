0c33259ef4867a45107715b31898469254c4b4bc
Renan Sigolo <renan.sigolo@gmail.com>
2020-07-02 11:47:39 +1000

    refact: adjust structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from '@material-ui/core'
+import { Box, Container, Typography, Grid } from '@material-ui/core'
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import logo from '../logo.svg'
@@ -6,12 +6,19 @@ import logo from '../logo.svg'
 export default class Hero extends React.Component {
   render() {
     return (
-      <Container maxWidth="sm">
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <img src={logo} className="hero-logo" alt="logo" />
-        </Box>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className="hero-bg"
+      >
+        <Container maxWidth="sm">
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <img src={logo} className="hero-logo" alt="logo" />
+          </Box>
 
-        {/* <Typography
+          {/* <Typography
           variant="h3"
           component="h1"
           color="inherit"
@@ -21,19 +28,20 @@ export default class Hero extends React.Component {
           <FormattedMessage id="hero.title" defaultMessage="MyADHD" />
         </Typography> */}
 
-        <Typography
-          variant="h3"
-          component="h1"
-          color="inherit"
-          align="center"
-          gutterBottom
-        >
-          <FormattedMessage
-            id="hero.subtitle"
-            defaultMessage="The first ADHD app with cognitive analysis!"
-          />
-        </Typography>
-      </Container>
+          <Typography
+            variant="h3"
+            component="h1"
+            color="inherit"
+            align="center"
+            gutterBottom
+          >
+            <FormattedMessage
+              id="hero.subtitle"
+              defaultMessage="The first ADHD app with cognitive analysis!"
+            />
+          </Typography>
+        </Container>
+      </Grid>
     )
   }
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Download from '../components/Download'
+import Features from '../components/Features'
+import Hero from '../components/Hero'
+
+const User = (props) => <div></div>
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <Hero />
+
+      <Download />
+
+      <Features />
+
+      {/* <Highlights /> */}
+
+      {/* <About /> */}
+
+      {/* <Contact /> */}
+    </React.Fragment>
+  )
+}
