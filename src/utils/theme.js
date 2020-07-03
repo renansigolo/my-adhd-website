f90d1d028bf3bff8751e0ef11a4f237c262e1e04
Renan Sigolo <renan.sigolo@gmail.com>
2020-07-03 14:15:24 +1000

    fix: theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,10 +2,7 @@
 import pink from '@material-ui/core/colors/pink'
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
-let theme = createMuiTheme()
-theme = responsiveFontSizes(theme)
-
-theme = createMuiTheme({
+let theme = createMuiTheme({
   palette: {
     primary: {
       main: pink[500],
@@ -13,4 +10,5 @@ theme = createMuiTheme({
   },
 })
 
+theme = responsiveFontSizes(theme)
 export default theme
