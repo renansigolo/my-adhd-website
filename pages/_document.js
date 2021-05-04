402709b8dad145039a9ee78b4a9c736457fc7751
Renan Sigolo <renan.sigolo@gmail.com>
2021-05-05 03:50:25 +1000

    feat: add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,34 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 import theme from '../theme'
 
+/**
+ * Add Google Analytics to Prod build
+ */
+function AddAnalytics() {
+  return (
+    <>
+      {process.env.NODE_ENV === 'production' ? (
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-82193310-7"
+          ></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag("js", new Date());
+
+                gtag("config", "UA-82193310-7");`,
+            }}
+          />
+        </Head>
+      ) : null}
+    </>
+  )
+}
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -14,6 +42,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <AddAnalytics />
         </Head>
         <body>
           <Main />
