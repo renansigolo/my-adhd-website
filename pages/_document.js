79acad2b157e2e58461c4fabb766725278f488c6
Renan Sigolo <renan.sigolo@gmail.com>
2021-05-05 03:27:12 +1000

    build: fix lang

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ import theme from '../theme'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
