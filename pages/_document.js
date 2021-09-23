3687546b3e15f62509ff8a88517ac87983087409
Renan Sigolo <renan.sigolo@gmail.com>
2021-09-23 17:32:32 +1000

    refact: isolate facebook component

--- pages/_document.js
+++ pages/_document.js
@@ -33,37 +33,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-          {/* <div id="fb-root"></div>
-
-          <div id="fb-customer-chat" className="fb-customerchat"></div>
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-      var chatbox = document.getElementById("fb-customer-chat")
-      chatbox.setAttribute("page_id", "105345684629745")
-      chatbox.setAttribute("attribution", "biz_inbox")
-
-      console.log("Init Facebook")
-
-      window.fbAsyncInit = function () {
-        FB.init({
-          xfbml: true,
-          version: "v11.0"
-        })
-      }
-      ;(function (d, s, id) {
-        var js,
-          fjs = d.getElementsByTagName(s)[0]
-        if (d.getElementById(id)) return
-        js = d.createElement(s)
-        js.id = id
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"
-        fjs.parentNode.insertBefore(js, fjs)
-      })(document, "script", "facebook-jssdk")
-    `
-            }}
-          /> */}
           <Main />
           <NextScript />
         </body>
