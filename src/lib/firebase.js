bd0d7edb19f5cfe8829f155ab8b72451bf442c1b
Renan Sigolo <renan.sigolo@gmail.com>
2022-08-04 17:44:49 +1000

    fix: firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,28 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { getApp, initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
-// Initialize Firebase
-function createFirebaseApp() {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-  }
-
-  try {
-    return getApp()
-  } catch {
-    return initializeApp(firebaseConfig)
-  }
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
-const firebaseApp = createFirebaseApp()
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+// function createFirebaseApp() {
+//   try {
+//     return getApp()
+//   } catch {
+//     return initializeApp(firebaseConfig)
+//   }
+// }
+// const firebaseApp = createFirebaseApp()
 
 // Auth exports
 export const auth = getAuth(firebaseApp)
