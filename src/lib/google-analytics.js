f78dd341269c4a15daea10210a0e301efe67a386
Renan Sigolo <renan.sigolo@gmail.com>
2022-08-23 17:23:20 +1000

    build: update google analytics

---
+++ src/lib/google-analytics.js
@@ -0,0 +1,46 @@
+import Script from "next/script"
+export const GA_ID_WEB = process.env.NEXT_PUBLIC_GA_ID
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (/** @type {string} */ url) => {
+  // @ts-ignore
+  window.gtag("config", GA_ID_WEB, {
+    page_path: url
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  // @ts-ignore
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  })
+}
+
+/** Global Site Tag (gtag.js) - Google Analytics */
+export const initGoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID_WEB}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID_WEB}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
+    </>
+  )
+}
