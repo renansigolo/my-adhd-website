adfd70d76e8a619788de873799f8396bb4da1aa9
Renan Sigolo <renan.sigolo@gmail.com>
2022-08-23 17:28:27 +1000

    build: update google analytics (#60)

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -1,4 +1,4 @@
-import * as gtag from "@/lib/gtag"
+import * as gtag from "@/lib/google-analytics"
 // import { doc, getFirestore, onSnapshot } from "firebase/firestore"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
@@ -15,13 +15,16 @@ export const useUserLocale = () => {
   const router = useRouter()
   const { locale, defaultLocale } = router
   const messages = languages[locale.substring(0, 2)]
+
   useEffect(() => {
     const handleRouteChange = (/** @type {string} */ url) => {
       gtag.pageview(url)
     }
     router.events.on("routeChangeComplete", handleRouteChange)
+    router.events.on("hashChangeComplete", handleRouteChange)
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("hashChangeComplete", handleRouteChange)
     }
   }, [router.events])
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import { GA_TRACKING_ID } from "@/lib/gtag"
+import { initGoogleAnalytics } from "@/lib/google-analytics"
 import Document, { Head, Html, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
@@ -13,24 +13,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;800&display=swap"
             rel="stylesheet"
           />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-            }}
-          />
+          {initGoogleAnalytics()}
         </Head>
         <body>
           <Main />
