f18c48869ed1bcd0b4cb36ec7b9d6dda222cc4dd
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-01 22:09:31 +1000

    chore: update project config (#65)
    * chore: update tailwind
    
    * ci: update cypress condition in gha
    
    * ci: cleanup actions
    
    * ci: cleanup vercel commands
    
    * fix: vercel cli
    
    * ci: add concurrency to cypress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
-        // @ts-ignore
         sans: ['"Baloo 2"', ...defaultTheme.fontFamily.sans]
       }
     }
