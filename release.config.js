4418da2a6a53faded6e302d4e46ef4bb7aa53edb
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-05 13:21:01 +1000

    ci: update semantic release

--- release.config.js
+++ release.config.js
@@ -4,13 +4,12 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "package-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        assets: ["dist/**", "package.json", "package-lock.json", "CHANGELOG.md"]
       }
     ],
     "@semantic-release/github"
