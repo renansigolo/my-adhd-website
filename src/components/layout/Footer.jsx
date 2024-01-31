935b3a392b882f0c13e94ac3080d4fd216f32be2
Renan Sigolo <renan.sigolo@gmail.com>
2024-01-31 18:14:22 +1100

    fix: footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -75,8 +75,8 @@ export function Footer() {
                 id="privacy-policy.title"
               />
             </Link>
-            <span className="mx-2">|</span>
-            {/* <Link href="/support" className="hover:underline">
+            {/* <span className="mx-2">|</span>
+             <Link href="/support" className="hover:underline">
               <FormattedMessage defaultMessage="Support" id="faq.title" />
             </Link> */}
           </p>
