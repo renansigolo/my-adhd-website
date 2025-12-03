c9de8dbfc1ccd0f9002a40e75e8633cbfcec3d27
Renan Sigolo <renan.sigolo@gmail.com>
2025-12-04 00:31:59 +1100

    chore: bump to node 24

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import { Disclaimer } from "@/app/[locale]/(home)/Disclaimer"
 import { Download } from "@/app/[locale]/(home)/Download"
 import { Hero } from "@/app/[locale]/(home)/Hero"
 import { Highlights } from "@/app/[locale]/(home)/Highlights"
-import { Banner } from "@/components/Banner"
 import { Divider } from "@/components/Divider"
 import { setRequestLocale } from "next-intl/server"
 import { use } from "react"
@@ -17,7 +16,7 @@ export default function HomePage({ params }: PageProps<"/[locale]">) {
   return (
     <>
       <main>
-        <Banner />
+        {/* <Banner /> */}
 
         <Hero />
 
